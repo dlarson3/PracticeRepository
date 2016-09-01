038a29982326e06b8eda82399ca884e9d5bde5d6
Danielle Larson <daniellelarson@Danielles-MacBook-Pro-2.local>
2016-09-01 15:24:33 -0700

    new program (very simple)

---
+++ increment.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a, total ;
+  cout << "Input number to be incremented: " << endl;
+  cin >> a;
+  total = a + 1;
+  cout << "The new number is " << total << endl;
+  return 0;
+}
